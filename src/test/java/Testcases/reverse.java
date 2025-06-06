8df5b10e616eb057baeab4f39996c6c66dec89e5
Gurpreet <singhgurpreet56481@gmail.com>
2025-06-06 12:26:41 +0530

    second commit

---
+++ src/test/java/Testcases/reverse.java
@@ -0,0 +1,18 @@
+package Testcases;
+
+public class reverse {
+
+	public static void main(String[] args) {
+		
+		class ReverseString {
+		    public void main(String[] args) {
+		        String original = "OpenAI";
+		        String reversed = new StringBuilder(original).reverse().toString();
+		        System.out.println("Reversed string: " + reversed);
+		    }
+		}
+
+
+	}
+
+}
